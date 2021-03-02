b33b2c20b38a816e06039835db14bd9b2674daaa
huyldth1904081 <huyldth1904081@fpt.edu.vn>
2021-03-02 23:07:12 +0700

    init mvc project for read news web client

---
+++ ReadNewsWebClient/ReadNewsWebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReadNewsWebClient.MvcApplication" Language="C#" %>
---
+++ ReadNewsWebClient/ReadNewsWebClient/Models/Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadNewsWebClient.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Source { get; set; }
+        public string Link { get; set; }
+        public string ImgUrls { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ArticleStatus Status { get; set; }
+        public enum ArticleStatus
+        {
+            PENDING = 0,
+            DELETED = -1,
+            ACTIVE = 1
+        }
+        //foreing key
+        public int CategoryId { get; set; }
+
+    }
+}
